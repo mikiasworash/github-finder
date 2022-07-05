8a59a3603632019e90e39e36a70f7e94954dd8b1
Mikias <mikias.worash.a@gmail.com>
2022-07-05 10:34:06 +0300

    Moved to Context

---
+++ src/components/users/UserResults.jsx
@@ -0,0 +1,24 @@
+import { useEffect, useContext } from "react";
+import Spinner from "../layout/Spinner";
+import UserItem from "./UserItem";
+import GithubContext from "../../context/github/GithubContext";
+
+function UserResults() {
+  const { users, loading, fetchUsers } = useContext(GithubContext);
+
+  useEffect(() => {
+    fetchUsers();
+  }, []);
+
+  return loading ? (
+    <Spinner />
+  ) : (
+    <div className="grid grid-cols-1 gap-8 xl:grid-cols-4 lg:grid-cols-3 md:grid-cols-2">
+      {users.map((user) => (
+        <UserItem key={user.id} user={user} />
+      ))}
+    </div>
+  );
+}
+
+export default UserResults;
