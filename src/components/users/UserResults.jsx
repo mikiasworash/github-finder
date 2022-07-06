0ea14b84900a421718201f321a7b95c0023298df
Mikias <mikias.worash.a@gmail.com>
2022-07-06 15:38:01 +0300

    Added Search Component

--- src/components/users/UserResults.jsx
+++ src/components/users/UserResults.jsx
@@ -1,14 +1,15 @@
-import { useEffect, useContext } from "react";
+import { useContext } from "react";
 import Spinner from "../layout/Spinner";
 import UserItem from "./UserItem";
 import GithubContext from "../../context/github/GithubContext";
 
 function UserResults() {
-  const { users, loading, fetchUsers } = useContext(GithubContext);
+  const { users, loading } = useContext(GithubContext);
 
-  useEffect(() => {
-    fetchUsers();
-  }, []);
+  // Get initial sample users (testing purpose)
+  // useEffect(() => {
+  //   fetchUsers();
+  // }, []);
 
   return loading ? (
     <Spinner />
